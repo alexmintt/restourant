72e1dfc6c0174c30db1dd89c6b2a72b38e3ba816
alexmintt <filatova.alex04@gmail.com>
2023-06-30 19:24:56 +0400

    dish

---
+++ restourant/src/Dish.java
@@ -0,0 +1,16 @@
+public class Dish {
+    private int id;
+    private String name;
+    public Dish (int id,  String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
