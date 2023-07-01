f277fd6e23e316b682f028933507ff0a88503468
alexmintt <filatova.alex04@gmail.com>
2023-07-01 13:42:57 +0400

    Ingredients_Dishes v2

--- restourant/src/Ingredients_Dishes.java
+++ restourant/src/Ingredients_Dishes.java
@@ -1,19 +1,19 @@
 public class Ingredients_Dishes {
     private int id;
-    private int ingredient_id;
-    private int dish_id;
+    private String ingredient_name;
+    private String dish_name;
     private int needed_amount;
-    public Ingredients_Dishes (int id, int ingredient_id, int dish_id, int needed_amount){
+    public Ingredients_Dishes (int id, String ingredient_name, String dish_name, int needed_amount){
         this.id = id;
-        this.ingredient_id = ingredient_id;
-        this.dish_id = dish_id;
+        this.ingredient_name = ingredient_name;
+        this.dish_name = dish_name;
         this.needed_amount = needed_amount;
     }
 
     public int getId() {
         return id;
     }
-    public int getII(){return ingredient_id;}
-    public int getDI(){return dish_id;}
+    public String getIN(){return ingredient_name;}
+    public String getDN(){return dish_name;}
     public int getNA(){return needed_amount;}
 }
